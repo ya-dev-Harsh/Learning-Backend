03b66afcb22eccbe42d7d74ca1032b702c544410
ya-dev-Harsh <yadav.hk05@gmail.com>
2025-09-18 00:07:48 +0530

    updated practice project

--- 6.1_Blog_API_Project/index.js
+++ 6.1_Blog_API_Project/index.js
@@ -67,7 +67,8 @@ app.post("/posts", (req, res) => {
 })
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
 app.patch("/posts/:id", (req, res) => {
-  const post = posts.find((p) => p.id === parseInt(req.params.id));
+  const id = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === id);
   if (!post) return res.status(404).json({ message: "Post not found" });
 
   if (req.body.title) post.title = req.body.title;
@@ -78,7 +79,8 @@ app.patch("/posts/:id", (req, res) => {
 });
 //CHALLENGE 5: DELETE a specific post by providing the post id.
 app.delete("/posts/:id", (req, res) => {
-  const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
+  const id = parseInt(req.params.id);
+  const index = posts.findIndex((p) => p.id === id);
   if (index === -1) return res.status(404).json({ message: "Post not found" });
 
   posts.splice(index, 1);
