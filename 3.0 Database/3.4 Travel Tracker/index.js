73f6ad7329e00180438e1a8627b6a8b94f80c32b
ya-dev-Harsh <yadav.hk05@gmail.com>
2025-10-13 23:56:49 +0530

    Task 2 done

--- 3.0 Database/3.4 Travel Tracker/index.js
+++ 3.0 Database/3.4 Travel Tracker/index.js
@@ -1,31 +1,56 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from'pg';
+import pg from "pg";
+
+const app = express();
+const port = 3000;
 
 const db = new pg.Client({
-  user:"postgres",
+ user:"postgres",
   host:"localhost",
   database:"World",
   password:"Harsh_2323490",
   port: 5432
 });
-
-const app = express();
-const port = 3000;
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-db.connect();
-
-app.get("/", async (req, res) => {
+async function checkVisisted() {
   const result = await db.query("SELECT country_code FROM visited_country");
+
   let countries = [];
   result.rows.forEach((country) => {
-    countries.push(country.country_code)
+    countries.push(country.country_code);
   });
-  res.render("index.ejs", { countries: countries, total: countries.length })
-  db.end();
+  return countries;
+}
+
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM books WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visited_country (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
 });
 
 app.listen(port, () => {
