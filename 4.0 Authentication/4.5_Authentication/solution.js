dc816b8d5402274bbbb91a188b29d49a66217d3c
ya-dev-Harsh <yadav.hk05@gmail.com>
2025-12-13 12:36:46 +0530

    O auth done

--- 4.0 Authentication/4.5_Authentication/solution.js
+++ 4.0 Authentication/4.5_Authentication/solution.js
@@ -20,7 +20,6 @@ app.use(
     saveUninitialized: true,
   })
 );
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -58,7 +57,6 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
-
   if (req.isAuthenticated()) {
     res.render("secrets.ejs");
   } else {
@@ -66,21 +64,20 @@ app.get("/secrets", (req, res) => {
   }
 });
 
-app.get("/auth/google", passport.authenticate("google", {
-  scope: ["profile", "email"]
-}));
-
-app.get("/auth/google/secrets", passport.authenticate("google", {
-  successRedirect: "/secrets",
-  failureRedirect: "/login",
-}))
-
-app.get("/logout", (req, res) => {
-  req.logout((err) => {
-    if(err) console.log(err);
-    res.redirect("/");
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
   })
-})
+);
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    successRedirect: "/secrets",
+    failureRedirect: "/login",
+  })
+);
 
 app.post(
   "/login",
@@ -124,25 +121,23 @@ app.post("/register", async (req, res) => {
 });
 
 passport.use(
+  "local",
   new Strategy(async function verify(username, password, cb) {
     try {
       const result = await db.query("SELECT * FROM userss WHERE email = $1 ", [
-        username,
+        profile, email
       ]);
       if (result.rows.length > 0) {
         const user = result.rows[0];
         const storedHashedPassword = user.password;
         bcrypt.compare(password, storedHashedPassword, (err, valid) => {
           if (err) {
-            //Error with password check
             console.error("Error comparing passwords:", err);
             return cb(err);
           } else {
             if (valid) {
-              //Passed password check
               return cb(null, user);
             } else {
-              //Did not pass password check
               return cb(null, false);
             }
           }
@@ -156,15 +151,17 @@ passport.use(
   })
 );
 
-passport.use("google", new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
-  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-},
-  async (accessToken, refreshToken, profile, cb) => {
-    console.log(profile);
-    try {
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    async (accessToken, refreshToken, profile, cb) => {
+      try {
         console.log(profile);
         const result = await db.query("SELECT * FROM userss WHERE email = $1", [
           profile.email,
@@ -176,17 +173,18 @@ passport.use("google", new GoogleStrategy({
           );
           return cb(null, newUser.rows[0]);
         } else {
-          //Already existing user 
           return cb(null, result.rows[0]);
         }
       } catch (err) {
         return cb(err);
       }
-  }))
-
+    }
+  )
+);
 passport.serializeUser((user, cb) => {
   cb(null, user);
 });
+
 passport.deserializeUser((user, cb) => {
   cb(null, user);
 });
